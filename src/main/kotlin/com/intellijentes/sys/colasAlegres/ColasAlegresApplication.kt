5aea36ae0650ab79b93cc09a4a5e9df31658658b
MarianaNava <mariana.nava@ciencias.unam.mx>
2026-02-17 12:57:09 -0600

    Base del proyecto

---
+++ src/main/kotlin/com/intellijentes/sys/colasAlegres/ColasAlegresApplication.kt
@@ -0,0 +1,11 @@
+package com.intellijentes.sys.colasAlegres
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ColasAlegresApplication
+
+fun main(args: Array<String>) {
+	runApplication<ColasAlegresApplication>(*args)
+}
---
+++ src/test/kotlin/com/intellijentes/sys/colasAlegres/ColasAlegresApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.intellijentes.sys.colasAlegres
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ColasAlegresApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
