f1435ad94daa7aa4b0650667b62bf02035a7a2a1
CaelusDLR <caelusdlr@gmail.com>
2026-02-24 14:56:48 -0600

    Función de extension para transdormar DTO en dominio

---
+++ src/main/kotlin/com/intellijentes/sys/colasAlegres/domain/AnimalEF.kt
@@ -0,0 +1,25 @@
+package com.intellijentes.sys.colasAlegres.domain
+
+import com.intellijentes.sys.colasAlegres.dto.request.CreateAnimalRequest
+import java.util.UUID
+
+/**
+ * Extension Fuction que convierte un DTO de tipo CreateAnimalRequest
+ * en un objeto de dominio Animal
+ */
+
+fun CreateAnimalRequest.toAnimal(): Animal {
+
+    // Generamos un identificador único de forma simulada
+    val id = "id-random-" + UUID.randomUUID().toString()
+
+    // Creamos el objeto de dominio usando los datos del DTO
+    return Animal(
+        id = id,
+        name = this.name,
+        description = this.description,
+        typeAnimal = this.typeAnimal,
+        animalBreed = this.animalBreed,
+        zipCodeFounded = this.zipCodeFounded
+    )
+}
