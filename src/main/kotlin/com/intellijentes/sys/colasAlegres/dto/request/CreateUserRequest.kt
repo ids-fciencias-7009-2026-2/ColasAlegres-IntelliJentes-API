65a7e4b793dd1c56377539b5d619ba2ef8807912
MarcoRG345 <tonyrayagarcia@gmail.com>
2026-02-24 00:46:47 -0600

    primeros dto añadidos, sujeto a cambios

--- src/main/kotlin/com/intellijentes/sys/colasAlegres/dto/request/CreateUserRequest.kt
+++ src/main/kotlin/com/intellijentes/sys/colasAlegres/dto/request/CreateUserRequest.kt
@@ -1,8 +1,12 @@
 package com.intellijentes.sys.colasAlegres.dto.request
 
+/**
+ * DTO mediante el cual recibimos informacion de "creacion de usuario"
+ * en la forma adecuada de transferencia.
+ * */
 data class CreateUserRequest (
     val name: String,
     val email: String,
     val hashPassword: String,
-    val zipCode: Int,
+    val zipCode: Int
     )
