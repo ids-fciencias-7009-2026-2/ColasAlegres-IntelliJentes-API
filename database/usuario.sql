541a8a6075af256e7fdcb2f066dd39c5f75543b5
lmrcolin <luismanuelrc@ciencias.unam.mx>
2026-03-02 00:23:35 -0600

    feat(db): update user table to match Kotlin data class

--- database/usuario.sql
+++ database/usuario.sql
@@ -1,7 +1,8 @@
-CREATE TABLE (
-    user_id INT PRIMARY KEY,
-    full_name VARCHAR(45),
-    zip_code VARCHAR(5),
-    password VARCHAR(45),
-    email VARCHAR(45)
+CREATE TABLE user (
+    id VARCHAR(36) PRIMARY KEY,
+    name VARCHAR(45) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    hash_password VARCHAR(255),
+    zip_code VARCHAR(10) NOT NULL,
+    is_active TINYINT(1) DEFAULT 1
 );
