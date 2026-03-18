b114e961dc93a0234b5a55ed520f2575a552f5cf
MarcoRG345 <tonyrayagarcia@gmail.com>
2026-03-17 23:44:16 -0600

    Arregla constructor UserEntity, valores predefinidos antes de instancia

--- src/main/kotlin/com/intellijentes/sys/colasAlegres/models/entities/UserEntity.kt
+++ src/main/kotlin/com/intellijentes/sys/colasAlegres/models/entities/UserEntity.kt
@@ -18,6 +18,6 @@ data class UserEntity(
     val id: String = UUID.randomUUID().toString(),
     val name: String = "",
     val email: String = "",
-    val password: String,
+    val password: String = "",
     val zipCode: String = ""
 )
